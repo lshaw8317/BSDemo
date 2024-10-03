626fc3180d5c7f08ad5ba0121c8a007952b2b567
lshaw8317 <61458341+lshaw8317@users.noreply.github.com>
2024-10-03 07:40:11 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,6 +6,7 @@ def gen_mat(order, exprs):
   Nullspace of the (transpose) matrix and the nullspace of the (transpose) matrix 
   with the column of constant terms deleted, give the linear combinations which sum to 0 
   and to a general constant respectively. If the latter set is larger, there are dependencies.
+  See "Generalized extrapolation methods based on compositions of a basic 2nd-order scheme", Blanes et. al. (2024).
   '''
     mat = sympy.Matrix.zeros(rows=len(exprs), cols=order+1)
     for i, p in enumerate(exprs):
