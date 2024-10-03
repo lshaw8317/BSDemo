48e254b841e57582334883334aaeb6f1769a1c2b
lshaw8317 <61458341+lshaw8317@users.noreply.github.com>
2024-10-03 07:38:47 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,10 @@ def gen_mat(order, exprs):
   '''
   Given list of mononomial sympy expressions `exprs`, generate matrix 
   of coefficients up to order `order`.
+  Converts a monomial into a row of a matrix of coefficients of the monomial terms. 
+  Nullspace of the (transpose) matrix and the nullspace of the (transpose) matrix 
+  with the column of constant terms deleted, give the linear combinations which sum to 0 
+  and to a general constant respectively. If the latter set is larger, there are dependencies.
   '''
     mat = sympy.Matrix.zeros(rows=len(exprs), cols=order+1)
     for i, p in enumerate(exprs):
